86fd632f7767c65be2b76f1b458934cf8475750e
probablyabot <anthonyrzhan@gmail.com>
2023-07-12 16:06:43 -0700

    refactoring

--- sdp.py
+++ sdp.py
@@ -4,9 +4,6 @@ from scipy.spatial import distance_matrix
 import gurobipy as gp
 from gurobipy import GRB
 from itertools import combinations
-from time import time
-import argparse
-from typing import Tuple
 
 
 # Given n points and k, uses semi-definite programming to produce a solution
@@ -69,6 +66,7 @@ def sample_from_ball(num_points, d, radius, center):
     return center + radius * (theta * r).T
 
 
+# Uses `sample_from_ball()` to generate given number of clusters
 def gen_clusters(num_clusters, points_per_cluster, d, radius, centers):
     clusters = [sample_from_ball(points_per_cluster, d, radius, centers[i])
                 for i in range(num_clusters)]
@@ -76,8 +74,8 @@ def gen_clusters(num_clusters, points_per_cluster, d, radius, centers):
 
 
 # Generate 2D coordinates for a regular n-gon inscribed in a circle of given
-# radius centered at the origin.
-def gen_polygon(n, radius=1):
+# radius centered at (cx, cy).
+def gen_polygon(n, radius=1, cx=0, cy=0):
     coordinates = []
     theta = 2 * np.pi / n
 
@@ -86,62 +84,15 @@ def gen_polygon(n, radius=1):
         y = radius * np.sin(theta * i)
         coordinates.append([x, y])
 
-    return np.array(coordinates)
+    return (cx, cy) + np.array(coordinates)
 
 
-# data = np.array([
-#     [0, 0],
-#     [1, 0],
-#     [0.5, np.sin(np.pi / 3.)]
-# ])
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-c', '--cluster', action='store_true')
-    parser.add_argument('-p', '--polygon', action='store_true')
-    parser.add_argument('-m', '--manual', action='store_true')
-    parser.add_argument('-nc', '--num_clusters', type=int, default=1)
-    parser.add_argument('-ppc', '--pts_per_cluster', type=int, default=10)
-    parser.add_argument('-d', '--dimension', type=int, default=2)
-    parser.add_argument('-r', '--radius', type=float, default=1.0)
-    parser.add_argument('-ns', '--num_sides', type=int, default=3)
-    parser.add_argument('-k', type=int, default=2)
-    parser.add_argument('-ng', '--no_gurobi', action='store_true')
-
-    args = parser.parse_args()
-    if args.cluster:
-        centers = []
-        for i in range(args.num_clusters):
-            center = []
-            while len(center) == 0:
-                center_raw = input(f'Cluster center #{i}: ').replace('(', '').replace(')', '')
-                center = [float(x) for x in center_raw.split(',')]
-                if len(center) != args.dimension:
-                    center = []
-            centers.append(center)
-        data = gen_clusters(args.num_clusters, args.pts_per_cluster,
-                            args.dimension, args.radius, centers)
-    elif args.polygon:
-        data = gen_polygon(args.num_sides, args.radius)
-    else:
-        pts = []
-        while True:
-            pt = input(f'Data point #{len(pts)+1} (return to stop): ').replace('(', '').replace(')', '')
-            if pt == '':
-                break
-            pts.append([float(x) for x in pt.split(',')])
-        data = np.array(pts)
-    k = args.k
-    print('Input data:\n', data)
-
-    start_t = time()
-    m, cost = sdp_k_means(data, k)
-    sdp_t = time()
-    if not args.no_gurobi:
-        opt = optimal_k_means(data, k)
-        opt_t = time()
-    print('SDP solver returned matrix:\n', np.around(m, 3))
-    print('SDP objective function value:', round(cost, 3))
-    print(f'SDP running time: {sdp_t - start_t} seconds')
-    if not args.no_gurobi:
-        print('Optimal objective function value:', round(opt, 3))
-        print(f'Gurobi running time: {opt_t - sdp_t} seconds')
+# Read in a d-dimension point from input and parse it into a Tuple[float] of
+# length d.
+def parse_point(prompt, expected_d):
+    while True:
+        pt_raw = input(prompt).strip('()')
+        pt = [float(x) for x in pt_raw.split(',')]
+        if len(pt) == expected_d:
+            return pt
+        print(f'Invalid point, expected dimension {expected_d}')
