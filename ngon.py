d9a69abb6deadf2f32ae1e878efbdde31d3eac86
probablyabot <anthonyrzhan@gmail.com>
2023-11-08 16:37:41 -0800

    fix beta constraint

--- ngon.py
+++ ngon.py
@@ -1,14 +1,17 @@
 import cvxpy as cp
 import numpy as np
 import matplotlib.pyplot as plt
+from itertools import combinations
 
-# function to generate a n-gon
+
+# function to generate an n-gon
 def gen_polygon(n, r=1, cx=0, cy=0):
     t = 2 * np.pi / n
     pts = [[r*np.cos(t*i), r*np.sin(t*i)] for i in range(n)]
-    return (cx, cy) + np.array(pts)
+    return [cx, cy] + np.array(pts)
 
-# creates the dual
+
+# creates and solves the dual
 def ngon_dual(n):
     pts = gen_polygon(n, n / (2 * np.pi))
 
@@ -17,27 +20,32 @@ def ngon_dual(n):
     d = [cp.Variable((n, n)) for _ in range(n)]
 
     constraints = [b >= 0]
-    constraints += [cp.diag(b) == 0]
+    constraints += [cp.diag(b) == 0]  # beta_{ii} = 0
     constraints += [d[i] >= 0 for i in range(n)]
     for i in range(n):
+        # set diagonal, i-th row, i-th col to be 0. these correspond
+        # to delta_{ijj}, delta_{iij}, delta_{iji}.
         constraints += [cp.diag(d[i]) == 0]
         constraints += [d[i][i] == 0]
         constraints += [d[i][:, i] == 0]
     for i in range(n):
+        # here we set lambda = n
         constraints.append(a + cp.sum(b[i]) + cp.sum(d[i]) <= n)
+    for i, j, k in combinations(range(n), 3):
+        # define delta_{ijk} for middle vertex i and j < k
+        constraints.append(d[i][k][j] == 0)
+        constraints.append(d[j][k][i] == 0)
+        constraints.append(d[k][j][i] == 0)
     for i in range(n-1):
         for j in range(i+1, n):
-            c = 2 * a - 2 * b[i][j]
+            c = 2 * a - b[i][j] - b[j][i]
             for k in range(n):
                 if k != i and k != j:
                     c += d[k][i][j]
-                    constraints.append(d[k][j][i] == 0)
-                    k1, k2 = sorted([i, k])
-                    c -= d[j][k1][k2]
-                    constraints.append(d[j][k2][k1] == 0)
-                    k1, k2 = sorted([j, k])
-                    c -= d[i][k1][k2]
-                    constraints.append(d[i][k2][k1] == 0)
+                    f, g = sorted([i, k])
+                    c -= d[j][f][g]
+                    f, g = sorted([j, k])
+                    c -= d[i][f][g]
             constraints.append(c <= np.linalg.norm(pts[i] - pts[j]) ** 2)
 
     prob = cp.Problem(cp.Maximize(a), constraints)
@@ -47,8 +55,8 @@ def ngon_dual(n):
 
 
 # takes in an n x n numpy array and creates a heatmap
-def make_heatmap(arr, title='Heatmap'):
-    plt.imshow(arr, cmap='coolwarm', interpolation='nearest')
+def make_heatmap(arr, cmin=None, cmax=None, title='Heatmap'):
+    plt.imshow(arr, cmap='coolwarm', interpolation='nearest', vmin=cmin, vmax=cmax)
     plt.colorbar()
     plt.title(title)
     plt.xticks(np.arange(0, n, 2))
@@ -60,7 +68,7 @@ def make_heatmap(arr, title='Heatmap'):
 n = 20
 a, b, d = ngon_dual(n)
 print('alpha:', np.round(a, 3))
-make_heatmap(b, f'betas, n={n}')
+make_heatmap(b, cmin=0, cmax=1, title=f'betas, n={n}')
 delta = np.zeros((n, n))
 for i in range(1, n - 1):
     for j in range(1, n - i):
@@ -69,4 +77,4 @@ for i in range(1, n - 1):
             k1, k2 = sorted([(k+i) % n, (k-j) % n])
             total += d[k][k1][k2]
         delta[i][j] = total
-make_heatmap(delta / n, f'deltas (averaged), n={n}')
+make_heatmap(delta / n, title=f'deltas (averaged), n={n}')
