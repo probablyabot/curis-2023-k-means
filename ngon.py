7b695734ac2dc0c94fcdd2e04311215725d5476d
probablyabot <anthonyrzhan@gmail.com>
2023-12-12 18:11:05 -0800

    refactoring, organize figures

--- ngon.py
+++ ngon.py
@@ -4,16 +4,18 @@ import matplotlib.pyplot as plt
 from itertools import combinations
 
 
-# function to generate an n-gon
-def gen_polygon(n, r=1, cx=0, cy=0):
+# generates a regular n-gon inscribed in circle of given radius
+def gen_ngon(n, r):
     t = 2 * np.pi / n
-    pts = [[r*np.cos(t*i), r*np.sin(t*i)] for i in range(n)]
-    return [cx, cy] + np.array(pts)
+    pts = [[np.cos(t*i), np.sin(t*i)] for i in range(n)]
+    return r * np.array(pts)
 
 
-# creates and solves the dual
-def ngon_dual(n):
-    pts = gen_polygon(n, n / (2 * np.pi))
+# produces a feasible solution to the dual lp by growing alpha
+# TODO: since we assume alpha is symmetric, might as well assume that
+# betas and deltas are symmetric as well
+def ngon_dual(n, r):
+    pts = gen_ngon(n, r)
 
     a = cp.Variable()
     b = cp.Variable((n, n))
@@ -30,6 +32,7 @@ def ngon_dual(n):
         constraints += [d[i][:, i] == 0]
     for i in range(n):
         # here we set lambda = n
+        # constraints.append(a + cp.sum(b[i]) <= n)
         constraints.append(a + cp.sum(b[i]) + cp.sum(d[i]) <= n)
     for i, j, k in combinations(range(n), 3):
         # define delta_{ijk} for middle vertex i and j < k
@@ -51,30 +54,36 @@ def ngon_dual(n):
     prob = cp.Problem(cp.Maximize(a), constraints)
     prob.solve()
 
+    # return a.value, b.value
     return a.value, b.value, [d[i].value for i in range(n)]
 
 
 # takes in an n x n numpy array and creates a heatmap
-def make_heatmap(arr, cmin=None, cmax=None, title='Heatmap'):
+def make_heatmap(arr, cmin=None, cmax=None, title='heatmap'):
     plt.imshow(arr, cmap='coolwarm', interpolation='nearest', vmin=cmin, vmax=cmax)
     plt.colorbar()
     plt.title(title)
-    plt.xticks(np.arange(0, n, 2))
-    plt.yticks(np.arange(0, n, 2))
     plt.gca().invert_yaxis()
     plt.show()
 
 
-n = 20
-a, b, d = ngon_dual(n)
-print('alpha:', np.round(a, 3))
-make_heatmap(b, cmin=0, cmax=1, title=f'betas, n={n}')
-delta = np.zeros((n, n))
-for i in range(1, n - 1):
-    for j in range(1, n - i):
-        total = 0
-        for k in range(n):
-            k1, k2 = sorted([(k+i) % n, (k-j) % n])
-            total += d[k][k1][k2]
-        delta[i][j] = total
-make_heatmap(delta / n, title=f'deltas (averaged), n={n}')
+# creates an n-gon instance, runs algorithm to produce a dual feasible
+# solution, then displays heatmap for betas and deltas
+def run(n, r=None):
+    if r is None:
+        r = n / (2 * np.pi)
+    a, b, d = ngon_dual(n, r)
+    print('alpha:', np.round(a, 3))
+    make_heatmap(b, title=f'betas, n={n}')
+    delta = np.zeros((n, n))
+    for i in range(1, n - 1):
+        for j in range(1, n - i):
+            total = 0
+            for k in range(n):
+                k1, k2 = sorted([(k+i) % n, (k-j) % n])
+                total += d[k][k1][k2]
+            delta[i][j] = total
+    make_heatmap(delta / n, title=f'deltas (averaged), n={n}')
+
+
+run(20, r=1)
